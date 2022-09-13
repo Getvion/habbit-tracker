5d7bd4d199cae803ac7e5e5527461f70da9023af
Oleg Somov <gog0125@mail.ru>
2022-09-13 10:38:17 +0300

    feat: add fonts and varables

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import { Empty } from '@common/empty';
 
 const App = () => (
-  <div>
+  <div className='app'>
     <Routes>
       <Route element={<Empty />} path='/' />
     </Routes>
--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 import 'modern-normalize/modern-normalize.css';
-import './index.scss';
+import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
