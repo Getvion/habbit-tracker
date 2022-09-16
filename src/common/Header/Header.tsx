059374b3bb07763dcf251b094ed8f18e2b0656b6
Oleg Somov <gog0125@mail.ru>
2022-09-16 10:59:59 +0300

    feat: fix date to correct format

--- src/common/Header/Header.tsx
+++ src/common/Header/Header.tsx
@@ -2,11 +2,22 @@ import React from 'react';
 
 import styles from './Header.module.scss';
 
-export const Header = () => (
-  <header className={styles.header}>
-    <span className={styles.header__date}>
-      {new Date().getDate()}/{new Date().getMonth()}/{new Date().getFullYear()}
-    </span>
-    <h1 className={styles.header__name}>Hi, UserName</h1>
-  </header>
-);
+export const Header = () => {
+  const dateOptions: Intl.DateTimeFormatOptions = {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+    weekday: 'short'
+  };
+
+  const date = new Intl.DateTimeFormat('ru', dateOptions).format(new Date());
+
+  return (
+    <header className={styles.header}>
+      <span className={styles.header__date}>{date}</span>
+      <h1 className={styles.header__name}>
+        Привет, <span>UserName!</span>
+      </h1>
+    </header>
+  );
+};
