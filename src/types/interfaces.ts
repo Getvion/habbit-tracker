bf0a49386d9f1dc240729809c2c137336d2a1d14
Oleg Somov <gog0125@mail.ru>
2022-09-19 09:22:29 +0300

    feat: create interfaces for calendar data

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -12,3 +12,26 @@ export interface IStore {
     habbitsList: IHabbit[];
   };
 }
+
+export interface ICalendarDay {
+  date: Date;
+  dayNumber: number;
+  day: string;
+  dayNumberInWeek: number;
+  dayShort: string;
+  year: number;
+  yearShort: string;
+  month: string;
+  monthShort: string;
+  monthNumber: number;
+  monthIndex: number;
+  timestamp: number;
+  week: number;
+}
+
+export interface ICalendarMonth {
+  month: string;
+  monthShort: string;
+  monthIndex: number;
+  date: Date;
+}
