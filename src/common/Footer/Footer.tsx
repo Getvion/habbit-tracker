7547bc70d9ad43a0fc7615c1bf231c802a79def3
Oleg Somov <gog0125@mail.ru>
2022-09-19 09:18:30 +0300

    feat: refactor footer, rewrite styles for fixed position

--- src/common/Footer/Footer.tsx
+++ src/common/Footer/Footer.tsx
@@ -1,28 +1,26 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import { SvgSelector } from '@assets/images/SvgSelector';
 
 import styles from './Footer.module.scss';
 
+const data = [
+  { link: '/', iconName: 'home' },
+  { link: '/calendar', iconName: 'calendar' },
+  { link: '/settings', iconName: 'settings' }
+];
+
 export const Footer = () => (
   <footer className={styles.footer}>
     <ul className={styles.footer__list}>
-      <li className={styles.list__item}>
-        <NavLink className={styles.list__link} to='/'>
-          <SvgSelector iconName='home' />
-        </NavLink>
-      </li>
-      <li className={styles.list__item}>
-        <NavLink className={styles.list__link} to='/calendar'>
-          <SvgSelector iconName='calendar' />
-        </NavLink>
-      </li>
-      <li className={styles.list__item}>
-        <NavLink className={styles.list__link} to='/settings'>
-          <SvgSelector iconName='settings' />
-        </NavLink>
-      </li>
+      {data.map(({ link, iconName }) => (
+        <li key={iconName} className={styles.list__item}>
+          <Link className={styles.list__link} to={link}>
+            <SvgSelector iconName={iconName} />
+          </Link>
+        </li>
+      ))}
     </ul>
   </footer>
 );
